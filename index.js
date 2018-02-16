13671ced1fa4341041f9507a5ce1fa3b24b90c24
Antério Vieira <anteriovieira@gmail.com>
2018-02-16 10:49:58 -0200

    Improvements

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 module.exports = {
   // Map of hooks
-  hooks: {},
+  hooks: {
+    "page:before": function (page) {
+      page.content = page.content.replace('/docs/{{version}}/', '')
+
+      return page
+    }
+  },
 
   // Map of new blocks
   blocks: {},
